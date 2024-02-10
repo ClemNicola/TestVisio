1672903d15f7688885031ca045cec03ab6e14afc
Clement Nicolas <clementnicolasprobis@gmail.com>
2024-02-10 13:49:01 +0100

    presque fix

--- app/javascript/calendar.js
+++ app/javascript/calendar.js
@@ -1,6 +1,5 @@
 const show = document.querySelector('.voir-moins')
 const arrow = document.querySelector('.fleche')
-const hours = document.querySelectorAll('.time')
 const columns = document.querySelectorAll('.day-selector')
 // const noneHours = document.querySelectorAll('.none-advisor-hours')
 
@@ -11,11 +10,25 @@ function showLess() {
 
 
   columns.forEach(column =>{
+
     const hours = column.querySelectorAll('.time');
+    const inputs = document.querySelectorAll('input')
+    const labels = document.querySelectorAll('label')
+
     const timeToggle = Array.from(hours).slice(10);
+    const inputToggle = Array.from(inputs).slice(10);
+    const labelToggle = Array.from(labels).slice(10);
 
     timeToggle.forEach(time => {
       time.style.display = isReduce ? "" : "none";
+    });
+
+    inputToggle.forEach(input => {
+      input.style.display = isReduce ? "" : "none";
+    });
+
+    labelToggle.forEach(label => {
+      label.style.display = isReduce ? "" : "none";
     });
   });
 
