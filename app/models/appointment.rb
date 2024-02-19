16325eaead39c3ad5e716d2f79fa76ac01659836
Clement Nicolas <clementnicolasprobis@gmail.com>
2024-02-19 17:40:05 +0100

    fix dashboard, reste la partie pending

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -8,6 +8,11 @@ class Appointment < ApplicationRecord
 
   enum status: { pending: 0, approved: 1, unapproved: 2 }
 
+  def end_time
+    advisor_time = Time.parse(advisor_hours)
+    advisor_time + appointment_type.duration.minutes
+  end
+
   def check_availability
     unless advisor.available_on?(date, advisor_hours, appointment_type_id)
       errors.add(:base, "Advisor is not available on the selected date and time")
